9973bee3e6237554588d25d8bcd611cca31c0042
Joao Matheus Bernardo Resende <joao.matheus92225@gmail.com>
2021-07-18 20:14:44 -0300

    estados funcionando

--- Assembly/main.asm
+++ Assembly/main.asm
@@ -5,9 +5,7 @@
 .def ADCL_aux = r20
 .def ADCH_aux = r21 
 .def PWM_value = r22
-.def soma = r23
-.def subt = r24
-
+.def PWM_valueL = r23
 .def pass = r25
 
 ;.def r26 = XL
@@ -24,7 +22,7 @@ rjmp setup
 ;------------------------- SETUP ---------------------------------
 
 Setup:
-	;---> CONFIGURANDO PORTAS DE SAIDA
+	;---> CONFIGURANDO SENHAS
 	ldi XL, 0x00 
 	ldi XH, 0x00 
 		
@@ -39,17 +37,19 @@ Setup:
 	ldi r16 , 0xff				; Defina todos os bits como 1
 	out DDRD, r16				; Defina todas as portas D como saida
 
-	ldi r16, 0xb8				; Defina os bits 3,4,5,7 como 1 
-	out DDRB, r16				; Defina as portas 3,4,5,7 como saida
+	Sbi DDRB, 3					; Defina como porta de saida
+	Sbi DDRB, 4					; Defina como porta de saida
+	Sbi DDRB, 5					; Defina como porta de saida
+	Sbi DDRB, 7					; Defina como porta de saida
 	
 	sbi DDRC, 5					; Porta para acionamento do mux
 	sbi DDRC, 6					; Porta para acionamento do mux
 
 	;---> CONFIGURANDO PORTAS DE ENTRADA
 	
-	ldi r16, 0x06				; Defina os bits 1 e 2 como 0
-	out DDRC, r16				; Defina como portas de entrada
-	
+	cbi DDRC, 2
+	cbi DDRC, 1
+
 	;---> CONFIGURANDO ADC
 		;PC0
 
@@ -64,17 +64,25 @@ Setup:
 	ldi r16, 0x83				; guarde 1000 0011 
 	sts 0xB0, r16				; TCCR2A- TIMER 2 - Fast PWM 
 	
-	ldi r16, 0x03				; guarde 1010 0011 
+	ldi r16, 0x03				; guarde 0000 0011 
 	sts 0xB1, r16				; TCCR2B - TIMER 2
     
 	ldi PWM_value, 0x7f			; 32 - 50, 7F - 127, FF - 255
+	ldi PWM_valueL, 0x00
 
 	;---> CONFIGURANDO TIMER
 
-    ldi r16, 0x05				; Ligar o timer, 0x05 Desativar
+    ldi r16, 0x0D				; Ligar o timer, 0x05 Desativar
     sts TCCR1B, r16				; Configurar o timer
-	sbi TIFR1, 1
+	
+	ldi r16, 0x00;0x1E        
+    sts OCR1AH, r16				; definindo o valor de TOP - HIGH
+	
+	ldi r16, 0x0A;0x84
+	sts OCR1AL, r16				; definindo o valor de TOP - LOW
 
+	;sbi TIFR1, 1				; Limpar Flag do timer
+	
 	rjmp Close
 
 ;------------------------- LOOP ---------------------------------
@@ -82,28 +90,27 @@ Close:
 	sbi PORTB, 5				; Liga led Vermelho
 	cbi PORTB, 4				; Desliga led azul
 	cbi PORTB, 3				; Desliga led verde
+	sts OCR2A, PWM_valueL
 	cbi	PORTB, 7				; Tranca fechada
 	
 	cbi PORTC, 5				; Padrao do display apagado
-	sbi PORTC, 6
-	
+	sbi PORTC, 6				; Seletor do MUX
+	clr cont					; Zera o contador
+
 	sbis PINC, 1				; Verifique se o pino ta em alto, true = salto
 	rjmp Close
 	rjmp Read_BP_ajuste
 	
 ajuste:
-	ldi soma, 0x00
-	ldi subt, 0x00
-
-	sbi TIFR1, 1
-
+	
 	cbi PORTB, 5				; Desliga led Vermelho
 	sbi PORTB, 4				; Liga led azul
 	cbi PORTB, 3				; Desliga led verde
+	sts OCR2A, PWM_valueL
 	cbi	PORTB, 7				; Tranca fechada
 
 	cbi PORTC, 5				; Padrao do display lendo o valor do potenciometro
-	cbi PORTC, 6
+	cbi PORTC, 6				; Seletor do MUX
 
 	;lendo adc
 	lds ADCL_aux ,ADCL			; Salve copias dos valores lidos
@@ -117,21 +124,20 @@ ajuste:
 
 	out PORTD, ADCL_aux			; saida de 8 bits BCD (8 bits)
 	out PORTC, ADCH_aux			; saida de 8 bits BCD (2 bits) 
+	nop
+	nop
 
 	sbic PINC, 2				; ADD Verifique se o pino ta em alto, true = salto
 	rjmp Read_BA_ajuste
 	nop
 	nop
-	nop
 
 	sbis PINC, 1				; Power Verifique se o pino ta em alto, true = salto
 	rjmp ajuste
-	rjmp Close
+	rjmp Read_BP_Close
 
 verificar:
 	inc cont					; Incrementa no contador
-    ldi r16, 0x05				; Ligar o timer, 0x05 Desativar
-    sts TCCR1B, r16				; Configurar o timer
 
 	cpi cont, 0x01				; compara se esta na primeira senha
 	breq VP1
@@ -142,21 +148,61 @@ verificar:
 	cpi cont, 0x03				; compara se esta na terceira senha
 	breq VP3
 
+VP1:
+	cpse ADCH_aux, XH			; se == skip, pois tem que ficar 0
+	rjmp errou					; se != continua
+	cpse ADCL_aux, XL			; se == skip, pois tem que ficar 0
+	rjmp errou					; se != continua
+
+	nop
+	sbi TIFR1, 1				; Limpar Flag do timer
+	rjmp timer
+
+VP2:
+	cpse ADCH_aux, YH			; se == skip, pois tem que ficar 0
+	rjmp errou					; se != continua
+	cpse ADCL_aux, YL			; se == skip, pois tem que ficar 0
+	rjmp errou					; se != continua
+
+	nop
+	sbi TIFR1, 1				; Limpar Flag do timer
+	rjmp timer
+
+VP3:
+	cpse ADCH_aux, ZH			; se == skip, pois tem que ficar 0
+	rjmp errou					; se != continua
+	cpse ADCL_aux, ZL			; se == skip, pois tem que ficar 0
+	rjmp errou					; se != continua
+
+	nop
+	sbi TIFR1, 1				; Limpar Flag do timer
+	rjmp timer
+
+Map:							; Forçar que qualquer valor maior que 999 seja 999
+	cpi ADCL_aux, 0xE7			; Compara para ver se é maior que 1000
+	brsh Force					; Caso o valor seja >=
+
+	out PORTD, ADCL_aux			; saida de 8 bits BCD (8 bits)
+	out PORTC, ADCH_aux			; saida de 8 bits BCD (2 bits) 
+	nop
+	nop
+
+	sbic PINC, 2				; ADD Verifique se o pino ta em alto, true = salto
+	rjmp Read_BA_ajuste
+	nop
+	nop
+
+	sbis PINC, 1				; Power Verifique se o pino ta em alto, true = salto
+	rjmp ajuste
+	rjmp Read_BP_Close			
+
 timer:
-	sbi PORTB, 3				; Liga led Verde
+	;sbi PORTB, 3				; Liga led Verde
+	sts OCR2A, PWM_value		;controla o duty cicle 50% 127 0x7F
 	cbi PORTB, 4				; Liga led azul
 	sbi PORTB, 5				; Desliga led Vermelho
 
-	;out OCR2A, PWM_value		;controla o duty cicle 50% 127 0x7F
 	
-	ldi r16, 0x0D				; Ligar o timer, 0x05 Desativar, 0x0D LIGAR
-    sts TCCR1B, r16				; Configurar o timer
-
-    ldi r16, 0x1E        
-    sts OCR1AH, r16				; definindo o valor de TOP - HIGH
-	
-	ldi r16, 0x84
-	sts OCR1AL, r16				; definindo o valor de TOP - LOW
 	
 	sbis TIFR1, 1				; TIFR1 - Quando o contador estourar, bit 1 = 1 
 	rjmp timer
@@ -169,6 +215,7 @@ open:
 	cbi PORTB, 5				; Desliga led Vermelho
 	cbi PORTB, 4				; Desliga led azul
 	sbi PORTB, 3				; liga led verde
+	sts OCR2A, PWM_value
 	sbi	PORTB, 7				; Tranca aberta
 	
 	sbi PORTC, 5				; Padrao dos --- no display
@@ -193,14 +240,11 @@ Read_BP_Close:
 	rjmp Read_BP_Close
 
 Read_BA_ajuste:
-	sbic PINC, 2				; Verifique se Continua em alto, true = salto
+	sbis PINC, 2				; Verifique se Continua em alto, true = salto
 	rjmp verificar
 	rjmp Read_BA_ajuste
 
-Map:							; Forçar que qualquer valor maior que 999 seja 999
-	cpi ADCL_aux, 0xE7			; Compara para ver se é maior que 1000
-	brsh Force					; Caso o valor seja >=
-	rjmp ajuste					; Caso o valor seja <
+
 
 Force:
 	ldi ADCL_aux, 0xE7			; Forçe o valor 999 no Registrador
@@ -216,30 +260,3 @@ compare:
 	tst pass
 	breq open
 	rjmp Close
-
-VP1:
-	cpse ADCH_aux, XH			; se == skip, pois tem que ficar 0
-	rjmp errou					; se != continua
-	cpse ADCL_aux, XL			; se == skip, pois tem que ficar 0
-	rjmp errou					; se != continua
-
-	nop
-	rjmp timer
-
-VP2:
-	cpse ADCH_aux, YH			; se == skip, pois tem que ficar 0
-	rjmp errou					; se != continua
-	cpse ADCL_aux, YL			; se == skip, pois tem que ficar 0
-	rjmp errou					; se != continua
-
-	nop
-	rjmp timer
-
-VP3:
-	cpse ADCH_aux, ZH			; se == skip, pois tem que ficar 0
-	rjmp errou					; se != continua
-	cpse ADCL_aux, ZL			; se == skip, pois tem que ficar 0
-	rjmp errou					; se != continua
-
-	nop
-	rjmp timer
