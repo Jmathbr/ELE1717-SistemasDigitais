e5fdbc352f0a86eb56cdb13010ac14eb6ea1ee2f
Joao Matheus Bernardo Resende <joao.matheus92225@gmail.com>
2021-07-31 18:52:40 -0300

    SPI ok

--- Assembly/Relogio/SPI/SPI/main.asm
+++ Assembly/Relogio/SPI/SPI/main.asm
@@ -7,6 +7,9 @@
 .def read = r18
 
 ; Replace with your application code
+
+; SPI comunication in between ATMEGA328p and MAX6902
+
 Setup:
 	
 	ldi r16, 0xFF			
@@ -72,39 +75,33 @@ loop_read:
 	out SPDR, r16				; Set comand
 	call Wait_Transmit_send
 
-	ldi r16, 0x00				; Value Seconds
-	out SPDR, r16				; Set comand
+	clr r16						; Clear R16
+
+	out SPDR, r16				; Value Seconds
 	call Wait_Transmit_send
 	
 	lds r17, 0x4E				; Write value SPDR in R17
 	
-	ldi r16, 0x02				; Value Minutis
-	out SPDR, r16				; Set comand
+	out SPDR, r16				; Value Minutis
 	call Wait_Transmit_send
 	
-	ldi r16, 0x12				; Value Hour
-	out SPDR, r16				; Set comand
+	out SPDR, r16				; Value Hour
 	call Wait_Transmit_send
 
-	ldi r16, 0x01				; Value Data
-	out SPDR, r16				; Set comand
+	out SPDR, r16				; Value Data
 	call Wait_Transmit_send
 
-	ldi r16, 0x08				; Value Month
-	out SPDR, r16				; Set comand
+	out SPDR, r16				; Value Month
 	call Wait_Transmit_send
 
-	ldi r16, 0x06				; Value Week Day
-	out SPDR, r16				; Set comand
+	out SPDR, r16				; Value Week Day
 	call Wait_Transmit_send
 
-	ldi r16, 0x21				; Value Year
-	out SPDR, r16				; Set comand
+	out SPDR, r16				; Value Year
 	call Wait_Transmit_send
 
-	ldi r16, 0x00				; Value Control
-	out SPDR, r16				; Set bit Protecty
-	call Wait_Transmit_send		; End Transmission
+	out SPDR, r16				; Set Control
+	call Wait_Transmit_send		
 
 	sbi PORTB, 2				; End Transmission
 	; END READ DATA
@@ -112,12 +109,11 @@ loop_read:
 
 
 Wait_Transmit_send:				; Wait 8 pulses clock 
-	in   r16, SPSR				;
+	in   r16, SPSR				
 	sbrs r16, SPIF				; Verify Flag interrupt
 	rjmp Wait_Transmit_send		; Ao final grava o valor recebido no SPDR
 	ret						
 
 Output:
-	;lds r16, 0x4E				; Write value SPDR in R16
 	out PORTD, r17
 	rjmp loop_read
