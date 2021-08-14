fd92472387d982e973a3d2f7ee7a49bb6c82b60a
Joao Matheus Bernardo Resende <joao.matheus92225@gmail.com>
2021-08-14 16:51:08 -0300

    Write 5 in position 32

--- C/LCD/LCD/main.c
+++ C/LCD/LCD/main.c
@@ -4,75 +4,85 @@
  * Created: 8/13/2021 7:15:26 PM
  *  Author: joaom
  */ 
-#define AUXPORTB PORTB
 
 #include <xc.h>
+#define F_CPU 8000000UL
+#include <avr/io.h>
+
+void delay_1(){
+	for(int i = 0;i<200;i++){}
+}
 void lcd_cmd(unsigned char cmd){
 	
-	PORTB &= 0xF0;
-	PORTB |= cmd;
-	
-	PORTC &= 0xFC;
-	PORTC |= 0x01;
-	for(int i = 0;i<255;i++){}//Delay
-	PORTC &=~ 0x01;
+	PORTB &= 0xF0;							// Mask preservation 4 LSBs
+	PORTB |= cmd;							// Add data command
+
+	PORTB |= (1<<PORTB4);					// Set E = 1
+	PORTB &= ~(1<<PORTB5);					// Set RS = 0
+	delay_1();				
+	PORTB &= ~(1<<PORTB4);					// Set E = 0 
 
 }
 
 void lcd_data(unsigned char data){
-	PORTC &= 0xFC;
-	PORTC |= 0x02;
-	for(int i=0; i<2 ;i++){
-		PORTB &= 0xF0;
-		PORTB |= data;
+	
+	PORTB |= (1<<PORTB5);					// Set RS = 1
+	
+	for(int i=0; i<2 ;i++){					// Send two data (2x 4 bits)
+		PORTB &= 0xF0;						// Mask preservation 4 LSBs
+		PORTB |= data;						// Add data
 		
-		//PORTC &= 0xFC;
-		PORTC |= 0x03;
-		for(int j = 0;j<255;j++){}//Delay
-		PORTC &=~ 0x01;	
+		PORTB |= (1<<PORTB4);				// Set E = 1
+		delay_1();
+		PORTB &= ~(1<<PORTB4);				// Set E = 0
 	}
-	PORTC &=~ 0x02;	
+	PORTB &= ~(1<<PORTB5);					// Set RS = 0 
 
 }
-/*void lcd_adress(unsigned char data){
-	lcd_cmd(data[0]);
-	lcd_cmd(data[4]);
-}*/
+void lcd_adress(unsigned char adress){
+	
+	PORTB &= ~(1<<PORTB5);					// Set RS = 0
+	
+	for(int i=0; i<2 ;i++){					// Send two data (2x 4 bits)
+		PORTB &= 0xF0;						// Mask preservation 4 LSBs
+		PORTB |= adress;						// Add data
+		
+		PORTB |= (1<<PORTB4);				// Set E = 1
+		delay_1();
+		PORTB &= ~(1<<PORTB4);				// Set E = 0
+	}
+
+}
 void lcd_init(){
-	//RS = 0 D7:D4 = 0010
-	//RS = 0 D7:D4 = 1000
-	lcd_cmd(0x02);
-	lcd_cmd(0x08);
+	
+	lcd_cmd(0x02);							//RS = 0 D7:D4 = 0010
+	lcd_cmd(0x08);							//RS = 0 D7:D4 = 1000
+	
 }
 
 void lcd_off_cursor(){
-	//RS = 0 D7:D4 = 0000
-	//RS = 0 D7:D4 = 1100
-	lcd_cmd(0x00);
-	lcd_cmd(0x0C);
+
+	lcd_cmd(0x00);							//RS = 0 D7:D4 = 0000
+	lcd_cmd(0x0C);							//RS = 0 D7:D4 = 1100
 }
 
 void lcd_on_cursor(){
-	//RS = 0 D7:D4 = 0000
-	//RS = 0 D7:D4 = 1100
-	lcd_cmd(0x00);
-	lcd_cmd(0x0F);
+	
+	lcd_cmd(0x00);							//RS = 0 D7:D4 = 0000
+	lcd_cmd(0x0F);							//RS = 0 D7:D4 = 1100
 }
 
-
-
-
-int main(void)
-{
+int main(void){
+	
 	DDRB = 0xff;
 	DDRC = 0xff;
 	PORTB = 0xF8;
 	PORTC = 0xAF;
+	
 	lcd_init();
 	lcd_off_cursor();
-	lcd_cmd(0x02);
-	lcd_cmd(0x02);
 	lcd_on_cursor();
-	lcd_data(0xAF);
+	lcd_adress(0x8F)
+	lcd_data(0x35);
     
 }
